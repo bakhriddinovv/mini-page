9bbd8472c6a078322dfaf570fa23c1ba196db0f7
bakhriddinovv <uzdarwin7@gmail.com>
2024-02-02 19:23:16 +0500

    first commit

---
+++ src/pages/landing/index.jsx
@@ -0,0 +1,203 @@
+import React from 'react';
+import {Fade , Zoom ,Slide} from "react-reveal"
+
+
+
+function Index(props) {
+    return (
+        <div className={"row"}>
+            <div className="col-md-6">
+                <Zoom bottom>
+                    <div className="card">
+                        <div className="card-header">
+                            <h1>Lorem ipsum dolor sit amet.</h1>
+                        </div>
+                        <div className="card-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet corporis debitis
+                                non nostrum, perferendis perspiciatis quam tempore, totam vel voluptas voluptates? </p>
+                        </div>
+                        <div className="card-footer">
+                            <button>Lorem ipsum.</button>
+                        </div>
+                    </div>
+
+                </Zoom>
+
+
+            </div>
+            <div className="col-md-6">
+                <Fade right>
+                    <div className="card">
+                        <div className="card-header">
+                            <h1>Lorem ipsum dolor sit amet.</h1>
+                        </div>
+                        <div className="card-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet corporis debitis
+                                non nostrum, perferendis perspiciatis quam tempore, totam vel voluptas voluptates? </p>
+                        </div>
+                        <div className="card-footer">
+                            <button>Lorem ipsum.</button>
+                        </div>
+                    </div>
+                </Fade>
+
+
+            </div>
+            <div className="col-md-6">
+                <div className="card">
+                    <div className="card-header">
+                        <h1>Lorem ipsum dolor sit amet.</h1>
+                    </div>
+                    <div className="card-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet corporis debitis
+                            non nostrum, perferendis perspiciatis quam tempore, totam vel voluptas voluptates? </p>
+                    </div>
+                    <div className="card-footer">
+                        <button>Lorem ipsum.</button>
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-6">
+                <div className="card">
+                    <div className="card-header">
+                        <h1>Lorem ipsum dolor sit amet.</h1>
+                    </div>
+                    <div className="card-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet corporis debitis
+                            non nostrum, perferendis perspiciatis quam tempore, totam vel voluptas voluptates? </p>
+                    </div>
+                    <div className="card-footer">
+                        <button>Lorem ipsum.</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-header">
+                            <h1>Lorem ipsum dolor sit amet.</h1>
+                        </div>
+                        <div className="card-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet corporis debitis
+                                non nostrum, perferendis perspiciatis quam tempore, totam vel voluptas voluptates? </p>
+                        </div>
+                        <div className="card-footer">
+                            <button>Lorem ipsum.</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-header">
+                            <h1>Lorem ipsum dolor sit amet.</h1>
+                        </div>
+                        <div className="card-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet corporis debitis
+                                non nostrum, perferendis perspiciatis quam tempore, totam vel voluptas voluptates? </p>
+                        </div>
+                        <div className="card-footer">
+                            <button>Lorem ipsum.</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-header">
+                            <h1>Lorem ipsum dolor sit amet.</h1>
+                        </div>
+                        <div className="card-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet corporis debitis
+                                non nostrum, perferendis perspiciatis quam tempore, totam vel voluptas voluptates? </p>
+                        </div>
+                        <div className="card-footer">
+                            <button>Lorem ipsum.</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-header">
+                            <h1>Lorem ipsum dolor sit amet.</h1>
+                        </div>
+                        <div className="card-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet corporis debitis
+                                non nostrum, perferendis perspiciatis quam tempore, totam vel voluptas voluptates? </p>
+                        </div>
+                        <div className="card-footer">
+                            <button>Lorem ipsum.</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-header">
+                            <h1>Lorem ipsum dolor sit amet.</h1>
+                        </div>
+                        <div className="card-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet corporis debitis
+                                non nostrum, perferendis perspiciatis quam tempore, totam vel voluptas voluptates? </p>
+                        </div>
+                        <div className="card-footer">
+                            <button>Lorem ipsum.</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-header">
+                            <h1>Lorem ipsum dolor sit amet.</h1>
+                        </div>
+                        <div className="card-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet corporis debitis
+                                non nostrum, perferendis perspiciatis quam tempore, totam vel voluptas voluptates? </p>
+                        </div>
+                        <div className="card-footer">
+                            <button>Lorem ipsum.</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-header">
+                            <h1>Lorem ipsum dolor sit amet.</h1>
+                        </div>
+                        <div className="card-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet corporis debitis
+                                non nostrum, perferendis perspiciatis quam tempore, totam vel voluptas voluptates? </p>
+                        </div>
+                        <div className="card-footer">
+                            <button>Lorem ipsum.</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-header">
+                            <h1>Lorem ipsum dolor sit amet.</h1>
+                        </div>
+                        <div className="card-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet corporis debitis
+                                non nostrum, perferendis perspiciatis quam tempore, totam vel voluptas voluptates? </p>
+                        </div>
+                        <div className="card-footer">
+                            <button>Lorem ipsum.</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+
+
+    );
+}
+
+export default Index;
